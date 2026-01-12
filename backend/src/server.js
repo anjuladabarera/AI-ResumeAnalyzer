8b15bfcb0777782a7766eace0467c775ea747f0b
anjuladabarera <wanjulasd@gmail.com>
2026-01-12 22:03:04 +0530

    Minor fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -25,23 +25,11 @@ connectDB();
 
 
 
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000');
+app.listen(PORT,()=>{
+console.log(`Server is running on port ${PORT}`);
 });
 
 
 
 
-// For Vercel serverless
-export default app;
 
-// For local development
-if (process.env.NODE_ENV !== 'production') {
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-}
-
-
-// mongodb+srv://wanjulasd_db_user:eBZe1XUbNxICGJbm@cluster0.4bpkums.mongodb.net/?appName=Cluster0
